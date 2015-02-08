d762bb68589d2127489564d8657cff66498af357
mcmoe <mcxmoe@gmail.com>
2015-02-09 00:34:00 +0100

    Implementing plot1 R script and submitting png plot

--- plot1.R
+++ plot1.R
@@ -1,4 +1,7 @@
 library(dplyr)
-data = tbl_df(read.csv(file = "household_power_consumption.txt", nrow = 69900, header = TRUE, sep = ";"))
+data = tbl_df(read.csv(file = "household_power_consumption.txt", nrow = 69900, sep = ";"))
 data = filter(data, Date == "1/2/2007" | Date == "2/2/2007")
+activePower = as.numeric(as.character(data$Global_active_power))
+activePower = na.omit(activePower)
 
+hist(activePower, col = "red", main = "Global Active Power", xlab = "Global Active Power (killowatts)")
