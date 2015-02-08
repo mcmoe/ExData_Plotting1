db59420358d28669c5fb403918953424a430cdfe
mcmoe <mcxmoe@gmail.com>
2015-02-09 00:36:08 +0100

    removing default Header parameter

--- plot2.R
+++ plot2.R
@@ -1,4 +1,4 @@
 library(dplyr)
-data = tbl_df(read.csv(file = "household_power_consumption.txt", nrow = 69900, header = TRUE, sep = ";"))
+data = tbl_df(read.csv(file = "household_power_consumption.txt", nrow = 69900, sep = ";"))
 data = filter(data, Date == "1/2/2007" | Date == "2/2/2007")
 
