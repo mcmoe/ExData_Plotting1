e0cfc0528544ee1b651df4d55daf0beefec66d92
mcmoe <mcxmoe@gmail.com>
2015-02-09 00:58:29 +0100

    Implementing plot2 script and png

--- plot2.R
+++ plot2.R
@@ -2,3 +2,8 @@ library(dplyr)
 data = tbl_df(read.csv(file = "household_power_consumption.txt", nrow = 69900, sep = ";"))
 data = filter(data, Date == "1/2/2007" | Date == "2/2/2007")
 
+activePower = as.numeric(as.character(data$Global_active_power))
+activePower = na.omit(activePower)
+data_date = as.POSIXct(paste(data$Date, data$Time), format = "%d/%m/%Y %H:%M:%S")
+
+plot(data_date, activePower, type = "l", xlab="", ylab="Global Active Power (killowatts)")
