1a2152726e07372d8224229b089e9fc903c8accf
mcmoe <mcxmoe@gmail.com>
2015-02-09 01:28:21 +0100

    Removing extra line from plot4.R

--- plot4.R
+++ plot4.R
@@ -9,6 +9,7 @@ activePower = na.omit(activePower)
 data_date = as.POSIXct(paste(data$Date, data$Time), format = "%d/%m/%Y %H:%M:%S")
 plot(data_date, activePower, type = "l", xlab="", ylab="Global Active Power (killowatts)")
 
+
 voltage = na.omit(as.numeric(as.character(data$Voltage)))
 plot(data_date, voltage, type = "l", xlab="dataetime", ylab="Voltage")
 
@@ -22,9 +23,9 @@ lines(data_date, submetering3, col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1), col=c("black","red", "blue"), )
 
 
-
 reactivePower = na.omit(as.numeric(as.character(data$Global_reactive_power)))
 plot(data_date, reactivePower, type = "l", xlab="datetime")
+
 
 dev.copy(png, file = "plot4.png")
 dev.off()
