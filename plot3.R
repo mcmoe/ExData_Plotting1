7f3580d6176a56791fc34b560812189fe0a3c954
mcmoe <mcxmoe@gmail.com>
2015-02-09 01:14:38 +0100

    Implementing plot3 script and png

--- plot3.R
+++ plot3.R
@@ -1,3 +1,18 @@
 library(dplyr)
 data = tbl_df(read.csv(file = "household_power_consumption.txt", nrow = 69900, sep = ";"))
 data = filter(data, Date == "1/2/2007" | Date == "2/2/2007")
+
+data_date = as.POSIXct(paste(data$Date, data$Time), format = "%d/%m/%Y %H:%M:%S")
+
+submetering1 = na.omit(as.numeric(as.character(data$Sub_metering_1)))
+submetering2 = na.omit(as.numeric(as.character(data$Sub_metering_2)))
+submetering3 = na.omit(as.numeric(as.character(data$Sub_metering_3)))
+
+plot(data_date, submetering1, type = "l", xlab="", ylab="Energy sub metering")
+lines(data_date, submetering2, col="red")
+lines(data_date, submetering3, col="blue")
+
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1), col=c("black","red", "blue"))
+
+dev.copy(png, file = "plot3.png")
+dev.off()
