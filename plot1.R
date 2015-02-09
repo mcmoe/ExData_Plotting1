fce28ecd0975b16263f173d6ee9a5e41e91a0579
mcmoe <mcxmoe@gmail.com>
2015-02-09 01:37:41 +0100

    overriding global mfrow for each plot script in case previously set

--- plot1.R
+++ plot1.R
@@ -1,4 +1,6 @@
 library(dplyr)
+
+par(mfrow = c(1,1))
 data = tbl_df(read.csv(file = "household_power_consumption.txt", nrow = 69900, sep = ";"))
 data = filter(data, Date == "1/2/2007" | Date == "2/2/2007")
 activePower = as.numeric(as.character(data$Global_active_power))
