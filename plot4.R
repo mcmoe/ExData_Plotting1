ffcde9309c9de0efae7304b12e582934914fe018
mcmoe <mcxmoe@gmail.com>
2015-02-09 01:33:44 +0100

    pushing unsaved changes in plot4.R

--- plot4.R
+++ plot4.R
@@ -20,7 +20,7 @@ submetering3 = na.omit(as.numeric(as.character(data$Sub_metering_3)))
 plot(data_date, submetering1, type = "l", xlab="", ylab="Energy sub metering")
 lines(data_date, submetering2, col="red")
 lines(data_date, submetering3, col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1), col=c("black","red", "blue"))
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1), col=c("black","red", "blue"), cex = 0.6, bty = "n")
 
 
 reactivePower = na.omit(as.numeric(as.character(data$Global_reactive_power)))
